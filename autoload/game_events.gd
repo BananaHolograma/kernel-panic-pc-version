b2d2a1590d60c3d787e5aade8e15d98ac6d7dde6
bananaholograma <bananaholograma@proton.me>
2024-02-23 13:00:08 +0000

    adjusted the options for the web

--- autoload/game_events.gd
+++ autoload/game_events.gd
@@ -20,10 +20,15 @@ signal hidden_pause_menu
 ### SAVE & LOAD ###
 signal deleted_saved_game(filename: String)
 
+@onready var screen_mode := DisplayServer.WINDOW_MODE_FULLSCREEN
+@onready var screen_resolution := Vector2(1280, 720)
+@onready var master_volume := 1.0
+@onready var music_volume := 0.8
+@onready var sfx_volume := 0.9
 
-static var screen_mode := DisplayServer.WINDOW_MODE_FULLSCREEN
-static var screen_resolution := Vector2(1280, 720)
-static var master_volume := 1.0
-static var music_volume := 0.8
-static var sfx_volume := 0.9
+
+func _ready():
+	if OS.get_name() == "Web":
+		screen_mode = DisplayServer.WINDOW_MODE_WINDOWED
+		screen_resolution = get_viewport().get_visible_rect().size
 
