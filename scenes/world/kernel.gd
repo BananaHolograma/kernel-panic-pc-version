cca8dcb4d2abae4776ebbe174757d94a6df0e366
bananaholograma <bananaholograma@proton.me>
2024-02-23 12:16:53 +0000

    kernel idle sound

--- scenes/world/kernel.gd
+++ scenes/world/kernel.gd
@@ -6,25 +6,27 @@ const BLUE_SCREEN = preload("res://scenes/world/blue_screen.tscn")
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var touch_sfx: AudioStreamPlayer = $TouchSFX
+@onready var loop: AudioStreamPlayer = $Loop
 @onready var implosion_particles: GPUParticles2D = $ImplosionParticles
 
 
 func _ready():
 	animation_player.play("idle")
 	
-	audio_stream_player.finished.connect(func():
+	touch_sfx.finished.connect(func():
 		get_tree().call_deferred("change_scene_to_packed", BLUE_SCREEN)
 	)
 
 
 func _on_area_2d_body_entered(body):
 	if body is Player:
+		loop.stop()
 		player_touched_kernel.emit()
 
 
 func _on_player_touched_kernel():
-	audio_stream_player.play()
+	touch_sfx.play()
 
 
 func _on_detection_area_body_entered(body):
